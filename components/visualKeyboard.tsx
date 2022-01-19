7ae7d3ec274d69d03b3a81418d14d5bca090d315
Michael Farrelly <mikefarrelly@gmail.com>
2022-01-18 21:32:10 -0700

    Add backspace and enter buttons

--- components/visualKeyboard.tsx
+++ components/visualKeyboard.tsx
@@ -19,9 +19,9 @@ export default function VisualKeyboard({
 
     const keys = React.useMemo(() => {
         return [
-            ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+            ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "Backspace"],
             ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-            ["Z", "X", "C", "V", "B", "N", "M"],
+            ["Z", "X", "C", "V", "B", "N", "M", "Enter"],
         ];
     }, []);
 
